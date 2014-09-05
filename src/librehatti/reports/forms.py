ed1293d7fc21035d6bfb4fa788569d301e10213d
Amitt Bhardwj <amitt.bhardwj@gmail.com>
2014-09-05 14:18:41 +0530

    Register Work Pending

--- src/librehatti/reports/forms.py
+++ src/librehatti/reports/forms.py
@@ -4,6 +4,7 @@ from search_choices import CLIENT_FIELD_CHOICES
 from search_choices import CLIENT_ORDER_CHOICES
 from search_choices import CONSTRAINT_CHOICES
 import datetime
+from django.forms.extras.widgets import SelectDateWidget
 
 """
 displays checkboxes for Client Search
@@ -15,7 +16,7 @@ class ClientForm(forms.Form):
 
 """
 displays chechboxes for Order Search
-"""        
+"""
 class OrderForm(forms.Form):
     order = forms.MultipleChoiceField(required=False,
     widget=forms.CheckboxSelectMultiple, choices=CLIENT_ORDER_CHOICES)
@@ -26,7 +27,15 @@ displays checkboxes for Constraints
 class AddConstraints(forms.Form):
     start_date = forms.DateField(required=False, initial='2014-01-01')
     end_date = forms.DateField(required=False, initial= datetime.date.today())
+    month = forms.DateField(required=False, widget=SelectDateWidget)
+    year = forms.DateField(required=False, widget=SelectDateWidget)
+    name = forms.CharField(required=False)
+    paid_service_tax = forms.IntegerField(required=False)
+    paid_education_tax = forms.IntegerField(required=False)
+    amount_given = forms.IntegerField(required=False)
+    material = forms.CharField(required=False)
+    type = forms.CharField(required=False)
+    lab = forms.CharField(required=False)
     additional_constraints = forms.MultipleChoiceField(required=False,
     widget=forms.CheckboxSelectMultiple, choices= CONSTRAINT_CHOICES)
-    #amount_greater_than = forms.FloatField(required=False, initial = 0)
-    #amount_less_than = forms.FloatField(required=False, initial = 1000000)
+    
--- src/librehatti/reports/search_choices.py
+++ src/librehatti/reports/search_choices.py
@@ -6,6 +6,8 @@ CLIENT_ORDER_CHOICES= ( ('discount', 'Discount'), ('debit', 'Debit'),
     ('mode of payment','Payment Mode'),
     )
 	          
-CONSTRAINT_CHOICES= ( ('date', 'Date'),#('gt', 'Amount greater than'),
-    #('lt', 'Amount lesser than'),
+CONSTRAINT_CHOICES= ( ('date', 'Date'),('month', 'Month'), ('year', 'Year'),
+    ('name', 'Name'), ('material', 'Material'), ('type', 'Type'), 
+    ('lab', 'Lab'), ('paid_service_tax', 'Paid service tax'), 
+    ('paid_education_tax', 'Paid education tax')
     )
